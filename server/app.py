e22913e9c795bf3f862828958df5190cc33b27a6
Sani Rogers <sanirogers138@gmail.com>
2023-03-22 13:01:56 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify,request
 from flask_migrate import Migrate
 
-from models import db, Vendor
+from models import db, Vendor, VendorSweet, Sweet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,27 +19,76 @@ def home():
 
 @app.route('/vendors')
 def vendors():
-    return ''
+    vendors = []
+
+    for vendor in Vendor.query.all():
+        vendors.append({
+            "id":vendor.id,
+            "name": vendor.name,
+        })
+    
+    return make_response(jsonify(vendors),200)
 
 @app.route('/vendors/<int:id>')
 def vendor_by_id(id):
-    return ''
+    vendor = Vendor.query.filter(Vendor.id == id).first()
+    if vendor == None:
+        return make_response(jsonify({"error":"Vendor not found"}),404)
+    vendor = {
+        "id": vendor.id,
+        "name": vendor.name,
+        "vendor_sweets": [vendor_sweet.to_dict() for vendor_sweet in VendorSweet.query.filter(VendorSweet.vendor_id == vendor.id)]
+    }
+    return make_response(jsonify(vendor), 200)
 
 @app.route('/sweets')
 def sweets():
-    return ''
+    sweets = []
+    for sweet in Sweet.query.all():
+        sweets.append(sweet.to_dict())
+    
+    return make_response(jsonify(sweets),200)
 
 @app.route('/sweets/<int:id>')
 def sweet_by_id(id):
-    return ''
+    sweet = Sweet.query.filter(Sweet.id == id).first()
+    if sweet == None:
+        return make_response(jsonify({"error":"Sweet not found"}),404)
+    sweet = sweet.to_dict()
+    return make_response(jsonify(sweet),200)
 
-@app.route('/vendor_sweets')
+@app.route('/vendor_sweets', methods = ["POST"])
 def vendor_sweets():
-    return ''
+    if request.method == "POST":
+        new_vendor_sweet = VendorSweet(
+            price=request.json["price"],
+            vendor_id=request.json["vendor_id"],
+            sweet_id=request.json["sweet_id"],
+        )
+        validation_errors = []
+        if new_vendor_sweet.vendor_id not in [vendor.id for vendor in Vendor.query.all()]:
+            validation_errors.append("Vendor does not exist")
+        if new_vendor_sweet.sweet_id not in [sweet.id for sweet in Sweet.query.all()]:
+            validation_errors.append("Sweet does not exist")
+        if len(validation_errors) == 0:
+            db.session.add(new_vendor_sweet)
+            db.session.commit()
+            return make_response(jsonify(new_vendor_sweet.to_dict()),200)
+        else:
+            return make_response(jsonify({"errors":validation_errors}),403)
 
-@app.route('/vendor_sweets/<int:id>')
+        
+@app.route('/vendor_sweets/<int:id>', methods =["DELETE"])
 def vendor_sweet_by_id(id):
-    return ''
+    if request.method == "DELETE":
+        vendor_sweet = VendorSweet.query.filter(VendorSweet.id == id).first()
+        if vendor_sweet == None:
+            return make_response(jsonify({"error":"VendorSweet not found"}),404)
+        db.session.delete(vendor_sweet)
+        db.session.commit()
+        return make_response(jsonify({}),200)
+    
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
@@ -11,7 +12,7 @@ db = SQLAlchemy(metadata=metadata)
 
 class Vendor(db.Model, SerializerMixin):
     __tablename__ = 'vendors'
-    serialize_rules = ('-sweets.vendors', 'vendor_sweets.vendor')
+    serialize_rules = ('-sweets.vendors', '-vendor_sweets.vendor',"-sweets.vendor")
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -33,6 +34,12 @@ class Sweet(db.Model, SerializerMixin):
     vendor_sweets = db.relationship('VendorSweet', backref='sweet')
     vendors = association_proxy('vendor_sweets', 'sweet', creator=lambda v: VendorSweet(vendor=v))
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
 class VendorSweet(db.Model, SerializerMixin):
     __tablename__ = 'vendor_sweets'
     serialize_rules = ('-sweet.vendor_sweets', '-vendor.vendor_sweets')
@@ -44,3 +51,20 @@ class VendorSweet(db.Model, SerializerMixin):
 
     vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
     sweet_id = db.Column(db.Integer, db.ForeignKey('sweets.id'))
+
+    def to_dict(self):
+        vendor_sweet = Sweet.query.filter(self.sweet_id == Sweet.id).first()
+        return {
+            "id": self.id,
+            "name": vendor_sweet.name,
+            "price": self.price,
+        }
+    
+    @validates("price")
+    def price_validation(self,key,price):
+        if price == "" or None:
+            raise ValueError("Price cannot be blank")
+        elif price < 0:
+            raise ValueError("Price cannot be a negative number")
+    
+
